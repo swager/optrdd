4a22c401ec4342866a8ccceb24b1088946b9b71f
Stefan Wager <stefan.wager@gmail.com>
2017-08-21 16:57:32 -0700

    Add some tests.

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -16,8 +16,6 @@ W = as.numeric(X >= threshold)
 Y = 10 + 20 * X + rnorm(n) + W
 
 # Test methods initially, and confirm gamma moments
-
-rdd.old = optrdd(X, Y=Y, max.second.derivative, max.window = 1)
 rdd.free = optrdd.new(X=X, Y=Y, W=W, max.second.derivative = max.second.derivative, verbose = FALSE)
 rdd.cate = optrdd.new(X=X, Y=Y, W=W, center = threshold, max.second.derivative = max.second.derivative, verbose = FALSE)
 
@@ -32,6 +30,23 @@ test_that("optrdd gammas satisfy constraints", {
     expect_equal(sum(rdd.free$gamma * X), 0, tolerance = tol)
 })
 
+# Test optimization strategies
+rdd.free.raw = optrdd.new(X=X, Y=Y, W=W, max.second.derivative = max.second.derivative, bin.width = 0.05, use.spline = FALSE, verbose = FALSE)
+rdd.cate.raw = optrdd.new(X=X, Y=Y, W=W, center = threshold, max.second.derivative = max.second.derivative, bin.width = 0.05, use.spline = FALSE, verbose = FALSE)
+rdd.free.qp = optrdd.new(X=X, Y=Y, W=W, max.second.derivative = max.second.derivative, optimizer = "quadprog")
+rdd.cate.qp = optrdd.new(X=X, Y=Y, W=W, center = threshold, max.second.derivative = max.second.derivative, optimizer = "quadprog")
+
+test_that("optimization strategies are equivalent", {
+    expect_equal(rdd.free$tau.hat, rdd.free.raw$tau.hat, tolerance = rdd.free$tau.plusminus)
+    expect_equal(rdd.free$tau.plusminus, rdd.free.raw$tau.plusminus, tolerance = 0.05)
+    expect_equal(rdd.free$tau.hat, rdd.free.qp$tau.hat, tolerance = 0.01)
+    expect_equal(rdd.free$tau.plusminus, rdd.free.qp$tau.plusminus, tolerance = 0.01)
+    
+    expect_equal(rdd.cate$tau.hat, rdd.cate.raw$tau.hat, tolerance = rdd.cate$tau.plusminus)
+    expect_equal(rdd.cate$tau.plusminus, rdd.cate.raw$tau.plusminus, tolerance = 0.05)
+    expect_equal(rdd.cate$tau.hat, rdd.cate.qp$tau.hat, tolerance = 0.01)
+    expect_equal(rdd.cate$tau.plusminus, rdd.cate.qp$tau.plusminus, tolerance = 0.01)
+})
 
 # Test sigma square estimation for optrdd
 rdd.fixed = optrdd.new(X=X, Y=Y, W=W, center = threshold, max.second.derivative = max.second.derivative, verbose=FALSE,
@@ -63,7 +78,6 @@ test_that("test plusminus function", {
 X.2d = cbind(X, runif(n, -1, 1))
 W = X.2d[, 1] < 0 | X.2d[, 2] < 0
 
-rdd.2d.old = optrdd.2d(X=X.2d, Y=Y, max.second.derivative)
 rdd.2d.free = optrdd.new(X=X.2d, Y=Y, W=W, max.second.derivative = max.second.derivative, verbose = FALSE)
 rdd.2d.cate = optrdd.new(X=X.2d, Y=Y, W=W, center = c(0, 0), max.second.derivative = max.second.derivative, verbose = FALSE)
 
@@ -81,18 +95,13 @@ test_that("2d-optrdd gammas satisfy constraints", {
     expect_equal(sum(rdd.2d.cate$gamma * W * X.2d[, 2]), 0, tolerance = tol)
 })
 
-rdd.2d.center = optrdd.2d(X.2d, Y = Y, max.second.derivative, estimate.cate.at.point = TRUE, 
-    center.treated.sample = TRUE)
-test_that("centered 2d-optrdd gammas satisfy constraints", {
-    expect_equal(sum(rdd.2d.center$gamma), 0)
-    expect_equal(sum(rdd.2d.center$gamma * W), 1)
-    expect_equal(sum(rdd.2d.center$gamma * X.2d[, 1]), 0)
-    expect_equal(sum(rdd.2d.center$gamma * X.2d[, 2]), 0)
-    expect_equal(sum(rdd.2d.center$gamma * X.2d[, 1] * W), 0)
-    expect_equal(sum(rdd.2d.center$gamma * X.2d[, 2] * W), 0)
+
+rdd.2d.free.raw = optrdd.new(X=X.2d, Y=Y, W=W, max.second.derivative = max.second.derivative, use.spline = FALSE, bin.width = 0.1, verbose = FALSE)
+
+test_that("optimization strategies are equivalent", {
+    expect_equal(rdd.2d.free$tau.hat, rdd.2d.free.raw$tau.hat, tolerance = rdd.2d.free$tau.plusminus)
+    expect_equal(rdd.2d.free$tau.plusminus, rdd.2d.free.raw$tau.plusminus, tolerance = 0.01)
 })
-
-
 
 
 # Test baseline methods
@@ -101,7 +110,7 @@ rectangle = llr(X, Y = Y, max.second.derivative, kernel = "rectangular",
 
 test_that("rectangular kernel gammas satisfy constraints", {
     half.bucket = min(rectangle$gamma.fun[-1, 1] -
-                          rectangle$gamma.fun[-nrow(rectangle$gamma.fun), 1])/2
+                          rectangle$gamma.fun[-nrow(rectangle$gamma.fun), 1])
     expect_equal(sum(rectangle$gamma), 0)
     expect_equal(sum(rectangle$gamma * (X > 0)), 1)
     expect_equal(sum(rectangle$gamma * X), 0, tolerance = half.bucket)
@@ -114,7 +123,7 @@ triangle = llr(X, Y = Y, max.second.derivative, kernel = "triangular",
 
 test_that("triangular kernel gammas satisfy constraints", {
     half.bucket = min(triangle$gamma.fun[-1, 1] -
-                          triangle$gamma.fun[-nrow(triangle$gamma.fun), 1])/2
+                          triangle$gamma.fun[-nrow(triangle$gamma.fun), 1])
     expect_equal(sum(triangle$gamma), 0)
     expect_equal(sum(triangle$gamma * (X > 0)), 1)
     expect_equal(sum(triangle$gamma * X), 0, tolerance = half.bucket)
@@ -123,40 +132,18 @@ test_that("triangular kernel gammas satisfy constraints", {
 
 
 test_that("relative performance of methods is as expected", {
-    expect_true(rdd$tau.plusminus < triangle$tau.plusminus)
+    expect_true(rdd.cate$tau.plusminus < triangle$tau.plusminus)
     expect_true(triangle$tau.plusminus < rectangle$tau.plusminus)
 })
 
 
-# Test aggregation for llr
-
-triangle.agg = llr(X.agg, Y = Y.agg, max.second.derivative, kernel = "triangular", 
-                   minimization.target = "mse", max.window = 1, num.samples = n.agg)
-
-test_that("aggregation for llr is roughly the same", {
-    expect_equal(triangle$tau.hat, triangle.agg$tau.hat, tolerance = 0.1)
-    expect_equal(triangle$tau.plusminus, triangle.agg$tau.plusminus, tolerance = 0.1)
-})
-
 # Test sigma square estimation for llr
-
-# If we do not estimate variance, then aggregation shouldn't do
-# anything
 triangle.fixed = llr(X, Y = Y, max.second.derivative, sigma.sq = 1, max.window = 1, 
                      use.homoskedatic.variance = TRUE, kernel = "triangular", minimization.target = "mse")
-triangle.agg.fixed = llr(X.agg, Y = Y.agg, max.second.derivative, sigma.sq = 1, 
-                         max.window = 1, num.samples = n.agg, use.homoskedatic.variance = TRUE, 
-                         kernel = "triangular", minimization.target = "mse")
 
 test_that("llr gets variance almost right", {
-    expect_equal(triangle$tau.hat, triangle.fixed$tau.hat, tolerance = 0.01)
+    expect_equal(triangle$tau.hat, triangle.fixed$tau.hat, tolerance = 0.05)
     expect_equal(triangle$tau.plusminus, triangle.fixed$tau.plusminus, 
-                 tolerance = 0.01)
+                 tolerance = 0.05)
 })
-
-test_that("aggregation for llr is exact when variance is known", {
-    expect_equal(triangle.fixed$tau.hat, triangle.agg.fixed$tau.hat)
-    expect_equal(triangle.fixed$tau.plusminus, triangle.agg.fixed$tau.plusminus)
-})
-
 
